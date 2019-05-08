1c6c9de960101d96212013136cc220b078c606ee
gracielajazmin <gracielajazmin_chavez@ucol.mx>
2019-05-08 08:35:10 -0500

    Updated

--- Distancia.js
+++ Distancia.js
@@ -1,12 +1,18 @@
-class Punto{
- constructor(puntos){
-     this.puntosdistancia = puntos;
- }
+class Punto {
+    constructor(puntos) {
+        this.puntosdistancia = puntos;
+    }
 
-    distancia(nuevopuntos){
-    this.puntosdistancia += nuevopuntos;
+    distancia() {
+       var x1 = document.getElementById('x').value;
+       var y1 = document.getElementById('y').value;
+       var x2 = document.getElementById('x2').value;
+       var y2 = document.getElementById('y2').value;
+        var total1 = (x2 - x1);
+        var total2 = (y2 - y1);
 
-    var total = this.puntosdistancia + ((x2-x1) + (y2-y1))
-    document.getElementById("resultado").textContent = total
+        var resultado = Math.sqrt(Math.pow(total1, 2) + Math.pow(total2, 2))
+        document.getElementById("resultado").innerHTML = resultado;
+        
     }
 }
